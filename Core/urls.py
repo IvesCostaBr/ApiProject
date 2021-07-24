599efea158002af41b75f27abe2635fd67a1f8c1
Ives Costa <ivespauiniam@gmail.com>
2021-07-23 20:49:56 -0500

    config env

--- Core/urls.py
+++ Core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('rest_framework.urls'))
 ]
